6bfc63d5929cc211df337f7c4d415de4986d05eb
Anu Sethi <anu1301@hotmail.co.uk>
2022-07-23 18:12:52 +0000

    Add ORM file, create classes and write session queries

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -4,7 +4,7 @@ import psycopg2
 # connect to "chinook" database
 connection = psycopg2.connect(database="chinook")
 
-# build a cursor (set or list similar to an array in javascript) object of the database
+# build a cursor object of the database
 cursor = connection.cursor()
 
 # Query 1 - select all records from the "Artist" table
@@ -19,11 +19,11 @@ cursor = connection.cursor()
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
 # cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
 
-# Query 5 - select only the albums with "ArtistId" #51 from the "Album" table
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
